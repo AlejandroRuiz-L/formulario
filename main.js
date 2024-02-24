1e2edbaac081d2612b2e63070d5e4bbff7906949
Alejandro <brayanalejandroruiz@gmail.com>
2024-02-23 21:08:03 -0500

    actualizo menejo de eventos

--- main.js
+++ main.js
@@ -4,19 +4,44 @@ const apellidos = document.getElementById("apellidos");
 const correo = document.getElementById("email");
 const tel = document.getElementById("tel");
 const skill = document.getElementById("skill");
+const evento_blur = function (elemento) {
+  elemento.addEventListener('blur', () => {
+    const patt_str = /^[a-zA-Z]{3}_[0-9]{5}$/;
+    if (patt_str.test(ficha.value)) {
+      console.log('valido');
+    }
+  })
+}
+/*
 function evento_blur(elemento) {
   elemento.addEventListener('blur', () => {
     if (elemento.value == "") {
-      alert("Debe llenar el campo.")
-    } else {
-      if (isNumber(elemento.value)) {
-        alert("Es correcto")
-      } else { "No es correcto" }
+      console.log("Debe llenar el campo.");
+      ficha.style.borderColor = "green";
     }
   })
 };
+ficha.addEventListener('blur', () => {
+  if (!patt_str.test(ficha.value)) {
+    console.log("Solo se pueden ingresar letras.");
+    ficha.style.borderColor = "red";
+  } else {
+    console.log("'Vacio' correcto.")
+  }
+})
+ficha.addEventListener('blur', () => {
+  if (!expresion1.test(ficha.value)) {
+    console.log("El campo 'ficha' no es válido.");
+    ficha.style.borderColor = "purple";
+  } else {
+    console.log("'Cod' correcto.");
+    ficha.style.borderColor = "skyblue";
+  }
+})
 evento_blur(ficha);
 evento_blur(nombre);
 evento_blur(apellidos);
 evento_blur(correo);
 evento_blur(tel);
+console.log('Finalizado');
+*/
